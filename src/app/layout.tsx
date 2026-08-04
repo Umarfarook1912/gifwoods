6e517e5a58ccc2a090e803a5e4c122db65e6fe6a
Umarfarook1912 <umarfarookj06@gmail.com>
2026-08-04 15:57:35 +0530

    google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "@/components/providers";
+import { GoogleAnalytics } from "@/components/shared/GoogleAnalytics";
 import { DEFAULT_SEO } from "@/constants/seo";
 
 export const metadata: Metadata = {
@@ -49,6 +50,7 @@ export default function RootLayout({
         }
         suppressHydrationWarning
       >
+        <GoogleAnalytics />
         <Providers>{children}</Providers>
       </body>
     </html>
